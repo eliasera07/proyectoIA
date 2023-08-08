14ac04e3ef53bc4f06ae8d011d25684d7cb2618b
Elias Renterias Arce <128107249+eliasera07@users.noreply.github.com>
2023-08-08 09:09:45 -0400

    martes8am

--- app.py
+++ app.py
@@ -29,6 +29,7 @@ def datos():
     edadT = int(request.form['edad'])
     generoT = str(request.form['genero'])
     nombreT = str(request.form['nombre'])
+    print("----------------------------------",pesoT, alturaT, edadT, generoT,nombreT,"----------------------------------1")
     return redirect(url_for('chat'))
 
 @app.route("/get_response", methods=["POST"])
@@ -37,7 +38,7 @@ def get_response():
     data = request.get_json()
     user_message = data["message"]
     prompt = f"You: {user_message}\nAssistant: "
-
+    print("----------------------------------",pesoT, alturaT, edadT, generoT,nombreT,"----------------------------------2")
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         temperature=0.4,
@@ -48,7 +49,7 @@ def get_response():
         messages = [
     {
         "role": "system",
-        "content": f'Tu eres un experto nutricionista, que guardaras estos datos para dar respuestas precisas a las preguntas. Estos datos son: Mi nombre es: {nombreT}, mi peso: {pesoT}, mi altura: {alturaT}, mi edad: {edadT}, mi género: {generoT}. Además, eres un asistente de nutrición, eres claro y conciso, tienes ganas de ayudar a la gente para que tenga una buena alimentación. Solo responderás consultas que tengan que ver con el área de nutrición. Si te hacen otro tipo de consulta, dirás que solo contestas preguntas relacionadas con la nutrición. Eres gentil y amable. Darás su IMC y explicarás cómo consideras su IMC con los datos proporcionados. Podrías recomendar una dieta si el usuario te lo pide. Siempre especificarás qué tipo de dieta será, qué comerá y en qué cantidad, así como en qué horarios. También podrías recomendar una rutina de ejercicios. Siempre preguntarás al usuario si asiste a un gimnasio o no. Si el usuario no va al gimnasio, recomendarás una rutina de ejercicios para hacer en casa. Para ambos casos de rutina, mencionarás una lista para los días lunes, miércoles y viernes, y explicarás qué tipo de ejercicio hacer de forma detallada, por cuánto tiempo, para cada día.'
+        "content": f'Tu eres un experto nutricionista, que guardaras mis datos para dar respuestas precisas a las preguntas. Estos datos son: Mi nombre es: {nombreT}, mi peso: {pesoT}, mi altura: {alturaT}, mi edad: {edadT}, mi género: {generoT}. Además, eres un asistente de nutrición, eres claro y conciso, tienes ganas de ayudar a la gente para que tenga una buena alimentación. Solo responderás consultas que tengan que ver con el área de nutrición. Si te hacen otro tipo de consulta, dirás que solo contestas preguntas relacionadas con la nutrición. Eres gentil y amable. Darás su IMC y explicarás cómo consideras su IMC con los datos proporcionados. Podrías recomendar una dieta si el usuario te lo pide. Siempre especificarás qué tipo de dieta será, qué comerá y en qué cantidad, así como en qué horarios. También podrías recomendar una rutina de ejercicios. Siempre preguntarás al usuario si asiste a un gimnasio o no. Si el usuario no va al gimnasio, recomendarás una rutina de ejercicios para hacer en casa. Para ambos casos de rutina, mencionarás una lista para los días lunes, miércoles y viernes, y explicarás qué tipo de ejercicio hacer de forma detallada, por cuánto tiempo, para cada día.'
     },
     {
         "role": "user",
@@ -58,7 +59,7 @@ def get_response():
     )
 
     assistant_message = response["choices"][0]["message"]["content"]
-
+    #print(assistant_message)
     return jsonify({"response": assistant_message})
 
 if __name__ == "__main__":
